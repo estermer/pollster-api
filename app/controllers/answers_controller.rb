22c5d8953b94c0d203d3f3d623b276a0c4a8f7b0
estermer <eric.stermer@gmail.com>
2016-11-08 17:23:33 -0500

    Added create answers method in answers controller to loop through the array of answers received from the front end and creates a new answer and adds it to the photo with the id passed to the method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,24 @@
 class AnswersController < ApplicationController
+  before_action :set_poll, only: [:create]
 
+  def create
+    params[:answers] do |text| #check to make sure that params[:answers] is right
+      answer = Answer.create(
+        text: text,
+        responses: 0,
+        poll_id: @poll.id
+      )
+    end
+
+    render json: {status: 200, poll: @poll}
+  end
+
+  def update
+  end
+
+  private
+
+  def set_poll
+    @poll = Poll.find(params[:id])
+  end
 end
