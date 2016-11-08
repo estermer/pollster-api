13c7a6114bbf14f3590ada9077e58e4d9f0a4edc
estermer <eric.stermer@gmail.com>
2016-11-08 17:03:36 -0500

    created a create method that created and saved a new poll based on validating that the data received is infact poll data based on the model
    responded with the new poll create if success and an invalid if not

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -4,8 +4,21 @@ class PollsController < ApplicationController
   end
 
   def create
+    poll = Poll.create(poll_params)
+
+    if poll.save
+      render json: {status: 200, poll: poll}
+    else
+      render json: {status: 400, response: "invalid data", poll: params}
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def poll_params
+    params.required(:poll).permit(:title, :question)
   end
 end
