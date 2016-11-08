218e12b0a293f6112c4146e5b89be0b8cec54960
estermer <eric.stermer@gmail.com>
2016-11-08 17:28:34 -0500

    add a rails route to update an answer in config

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,16 @@ Rails.application.routes.draw do
   resource :polls, only: [:show] do
     resource :answers, only: [:create]
   end
+  resource :answers, only: [:update]
 end
+
+
+# Prefix Verb   URI Pattern              Controller#Action
+#         root GET    /                        welcome#index
+#        polls GET    /polls(.:format)         polls#index
+#              POST   /polls(.:format)         polls#create
+#         poll DELETE /polls/:id(.:format)     polls#destroy
+# polls_answers POST   /polls/answers(.:format) answers#create
+#              GET    /polls(.:format)         polls#show
+#      answers PATCH  /answers(.:format)       answers#update
+#              PUT    /answers(.:format)       answers#update
