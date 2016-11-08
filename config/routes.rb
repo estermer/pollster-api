fb6e084ff4aaf172333c89fb01c7193bd5fd279d
estermer <eric.stermer@gmail.com>
2016-11-08 16:43:46 -0500

    Added routes to the route config
    for polls controller, added only index, create and destroy
    
    for answers, there is a create based upon the show of polls

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  resources :polls, only: [:index, :create, :destroy]
+  resource :polls, only: [:show] do
+    resource :answers, only: [:create]
+  end
 end
